b2f5e350acd23a83be3d30933961a0a4f34fc275
GillesAnneSophie <gilles.annesophiee@gmail.com>
2020-01-13 11:22:10 +0100

    Add SQL script to drop + config for DB connection

---
+++ test/src/persistence/config/drop_tables.sql
@@ -0,0 +1,12 @@
+DELETE FROM ride;
+DELETE FROM hotel;
+DELETE FROM site;
+DELETE FROM transport;
+DELETE FROM coordinates;
+
+
+DROP TABLE ride;
+DROP TABLE hotel;
+DROP TABLE site;
+DROP TABLE transport;
+DROP TABLE coordinates;
