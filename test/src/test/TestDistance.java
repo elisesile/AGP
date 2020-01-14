de19f37bc475c1cee468a4062fc6a7e790141d68
elise <elise@10.77.126.41>
2020-01-14 10:06:18 +0100

    Test pour la fonction de calcul de distance

---
+++ test/src/test/TestDistance.java
@@ -0,0 +1,11 @@
+package test;
+import moteur.ExcursionCalculator;
+
+public class TestDistance {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(moteur.ExcursionCalculator.getDistanceKM(-17.5402758,-149.5669825,-17.4942887,-149.4943525));
+	}
+	
+}
