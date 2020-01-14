8f73bfa58ea62091c59bd77cf497a822cbd7a12b
stevy24 <stevy24@users.noreply.github.com>
2020-01-14 09:40:24 +0100

    OfferEntry

---
+++ test/src/business/OfferEntry.java
@@ -0,0 +1,48 @@
+package business;
+
+public class OfferEntry {
+	 
+    private int minPrice = 500;
+    private int maxPrice = 5000;
+    private String typeOfTrip;
+    private int intensity = 2;
+    private String intensityMessage;
+    
+    public OfferEntry() {
+    	
+    }
+    
+	public int getMinPrice() {
+		return minPrice;
+	}
+	public void setMinPrice(int minPrice) {
+		this.minPrice = minPrice;
+	}
+	public int getMaxPrice() {
+		return maxPrice;
+	}
+	public void setMaxPrice(int maxPrice) {
+		this.maxPrice = maxPrice;
+	}
+	public String getTypeOfTrip() {
+		return typeOfTrip;
+	}
+	public void setTypeOfTrip(String typeOfTrip) {
+		this.typeOfTrip = typeOfTrip;
+	}
+	public int getIntensity() {
+		return intensity;
+	}
+	public void setIntensity(int intensity) {
+		this.intensity = intensity;
+	}
+	public String getIntensityMessage() {
+		return intensityMessage;
+	}
+	public void setIntensityMessage(String intensityMessage) {
+		this.intensityMessage = intensityMessage;
+	}
+    
+    
+
+}
