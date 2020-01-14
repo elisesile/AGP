f955c691d7f769c0c44d1f49e9bd02a28cb35510
stevy24 <stevy24@users.noreply.github.com>
2020-01-14 16:27:25 +0100

    OffreCalculator

---
+++ test/src/business/OfferCalculator.java
@@ -0,0 +1,25 @@
+package business;
+
+import java.util.ArrayList;
+
+import data.AbstractSite;
+import data.Excursion;
+import data.Offre;
+import data.Ride;
+
+public class OfferCalculator {
+
+	
+	private Offre offre ;
+	
+	public void initExcursions(int intensity){
+		int index;
+		for (index=0;index<=3;index++) {
+			Excursion excursion = new Excursion();
+			
+		}
+		
+	}
+	
+	
+}
