81a49bfc669fa0d5b1e686969c95821c3f3a9a1f
GillesAnneSophie <gilles.annesophiee@gmail.com>
2020-01-15 09:19:07 +0100

    fix some things in Process

--- test/src/persistence/QueryProcess.java
+++ test/src/persistence/QueryProcess.java
@@ -2,19 +2,13 @@ package persistence;
 
 import java.io.IOException;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
-import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
 
-import data.AbstractSite;
 import persistence.lucene.Indexer;
 import persistence.lucene.Searcher;
 
@@ -34,10 +28,12 @@ public class QueryProcess{
 	}
 	
 	
-	public void executeQuery(ResultSet sqlResult, String luceneParams) throws SQLException, CorruptIndexException, IOException {
+	public HashMap<Integer,HashMap<String, String>> executeQuery(ResultSet sqlResult, String luceneParams) throws SQLException, CorruptIndexException, IOException {
 		this.setSqlResult(sqlResult);
 		this.executeText(luceneParams);
 		this.joinQuery();
+		
+		return this.getResultHashMap();
 	}
 	
 	private void executeText(String luceneKeywords) {
@@ -46,56 +42,42 @@ public class QueryProcess{
 			this.setAlreadyIndexed(true);
 		}
 		
-//		Searcher searcher;
 		try {
-			searcher = new Searcher(this.getIndexer().getIndexDirectoryPath(), this.getIndexer().getSecondFieldName());
+			this.setSearcher(new Searcher(this.getIndexer().getIndexDirectoryPath(), this.getIndexer().getSecondFieldName()));
 			
 			try {
-				searcher.search(this.getIndexer().getMaxSearch(), luceneKeywords);
+				this.searcher.search(this.getIndexer().getMaxSearch(), luceneKeywords);
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		
-		// this.luceneQuerySavedResults = searcher.search(this.getIndexer().getMaxSearch(), luceneKeywords);
-		/*
-		searcher.initIterator();
-		ScoreDoc currentInfo;
-		while((currentInfo = searcher.nextIterator()) != null) {
-			//searcher.getDocument(currentInfo);
-			int currentId = searcher.getDocumentName(currentInfo, indexer.getFirstFieldName());
-			float currentScore = searcher.getDocumentScore(currentInfo);
-			System.out.println("currentId="+currentId+" currentScore="+currentScore);
-		}*/
 	}
 	
-	private HashMap<Integer,HashMap<String, String>> joinQuery() throws SQLException, CorruptIndexException, IOException {
+	private void joinQuery() throws SQLException, CorruptIndexException, IOException {
 		int foundDocuments = 0;
 		ScoreDoc actualScoreDoc;
-		resultHashMap = new HashMap<Integer,HashMap<String, String>>();
+		this.resultHashMap = new HashMap<Integer,HashMap<String, String>>();
 		
-//		ResultSetMetaData rsmd = sqlResult.getMetaData();
-		
-		while(sqlResult.next() && foundDocuments < searcher.getDocsIterator().length) {
-			while((actualScoreDoc = searcher.nextIterator()) != null) {
-				int fileName = Integer.valueOf(searcher.getDocumentName(actualScoreDoc, indexer.getFirstFieldName()));
-				if (sqlResult.getInt(1) == fileName) {
+		while(this.getSqlResult().next() && foundDocuments < this.getSearcher().getDocsIterator().length) {
+			while((actualScoreDoc = this.getSearcher().nextIterator()) != null) {
+				int fileName = Integer.valueOf(this.getSearcher().getDocumentName(actualScoreDoc, this.getIndexer().getFirstFieldName()));
+				if (this.getSqlResult().getInt(1) == fileName) {
 					HashMap<String,String> sitesInformations = new HashMap<String,String>();
 					
-					int id = sqlResult.getInt(1);
-					String name = sqlResult.getString(2);
-					String type = sqlResult.getString(3);
-					int price = sqlResult.getInt(4);
-					int id_coordinates= sqlResult.getInt(5);
+					int id = this.getSqlResult().getInt(1);
+					String name = this.getSqlResult().getString(2);
+					String type = this.getSqlResult().getString(3);
+					int price = this.getSqlResult().getInt(4);
+					int id_coordinates= this.getSqlResult().getInt(5);
 					
 					sitesInformations.put("name", name);
 					sitesInformations.put("type", type);
 					sitesInformations.put("price", String.valueOf(price));
 					sitesInformations.put("id_coordinates", String.valueOf(id_coordinates));
 					
-					resultHashMap.put(id, sitesInformations); 
+					this.resultHashMap.put(id, sitesInformations); 
 					
 					foundDocuments++;
 				}
@@ -103,15 +85,13 @@ public class QueryProcess{
 			
 			searcher.initIterator();
 		}
-		return resultHashMap;
-		
 	}
 	
-	public HashMap<Integer, HashMap<String, String>> getResultHashMap() {
+	private HashMap<Integer, HashMap<String, String>> getResultHashMap() {
 		return resultHashMap;
 	}
 
-	public void setResultHashMap(HashMap<Integer, HashMap<String, String>> resultHashMap) {
+	private void setResultHashMap(HashMap<Integer, HashMap<String, String>> resultHashMap) {
 		this.resultHashMap = resultHashMap;
 	}
 
@@ -183,5 +163,19 @@ public class QueryProcess{
 	 */
 	private void setIndexer(Indexer indexer) {
 		this.indexer = indexer;
+	}
+
+	/**
+	 * @return the searcher
+	 */
+	private Searcher getSearcher() {
+		return searcher;
+	}
+
+	/**
+	 * @param searcher the searcher to set
+	 */
+	private void setSearcher(Searcher searcher) {
+		this.searcher = searcher;
 	}
 }
