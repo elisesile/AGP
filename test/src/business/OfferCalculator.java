3e5347d236591590499b57f4a3bd9e8c6eeb4f21
stevy24 <stevy24@users.noreply.github.com>
2020-01-15 10:29:12 +0100

    Génération de liste

--- test/src/business/OfferCalculator.java
+++ test/src/business/OfferCalculator.java
@@ -13,12 +13,34 @@ public class OfferCalculator {
 	private Offre offre ;
 	
 	public void initExcursions(int intensity){
-		int index;
-		for (index=0;index<=3;index++) {
-			Excursion excursion = new Excursion();
-			
-		}
 		
+		ArrayList<Excursion> excursions = new ArrayList<Excursion>();
+		Excursion e1 = new Excursion();
+		Excursion e2 = new Excursion();
+		Excursion e3 = new Excursion();
+		Excursion e4 = new Excursion();
+		
+		switch(intensity) {
+		case 1:
+			e1.setBeach(true);
+			e2.setBeach(true);
+			e3.setBeach(true);
+			e4.setBeach(true);
+			break;
+		case 2:
+			e1.setBeach(true);
+			e2.setBeach(true);
+			break;
+		case 3:
+			e2.setBeach(true);
+			break;
+		default:break;
+		}
+		excursions.add(e1);
+		excursions.add(e2);
+		excursions.add(e3);
+		excursions.add(e4);
+		offre.setExcursions(excursions);
 	}
 	
 	
