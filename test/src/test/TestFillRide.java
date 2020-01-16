f4e2c32aeee1668280307ab1ee1438c6d2408e2e
Chri- <Chri-@LAPTOP-D3LVSR5A>
2020-01-16 15:15:30 +0100

    Fonction pour remplir automatiquement la table ride /!\ Vider la table ride en local avant de l'exécuter.

---
+++ test/src/test/TestFillRide.java
@@ -0,0 +1,15 @@
+package test;
+
+import persistence.jdbc.Queries;
+
+public class TestFillRide {
+
+	public static void main(String[] args) {
+		
+		Queries queries = new Queries();
+		
+		queries.fillRides();
+		
+	}
+	
+}
